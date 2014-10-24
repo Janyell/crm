6a104457fd2698188ead68661a7b0bab553554c6
megge <ria6@yandex.ru>
2014-10-24 16:50:38 +0400

    some edit

--- base_api/full_views/claim_views.py
+++ base_api/full_views/claim_views.py
@@ -232,13 +232,13 @@ def full_add_edit_claim(request):
                         new_claim_was_not_created = False
                         new_claim = Orders.objects.create(order_date=datetime.now(), client=client, role=role,
                                               unique_number=unique_number, company=company, bill=bill,
-                                              claim_status=bill_status, is_claim=is_claim,
+                                              bill_status=bill_status, is_claim=is_claim,
                                               account_number=account_number)
                     new_order_product_link = Order_Product.objects.create(order=new_claim, product=product,
                                                                           order_date=datetime.now(),
                                                                           count_of_products=count_of_products)
             if is_claim_create:
-                return HttpResponseRedirect('/orders/')
+                return HttpResponseRedirect('/claims/')
             else:
                 OrdersForm.base_fields['company'] = CompanyModelChoiceField(queryset=Companies.objects.filter(is_deleted=0),
                                                                             required=False)
