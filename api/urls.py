65b4891ed8aa1177a09a4df62857ea72589f73c5
megge <ria6@yandex.ru>
2015-02-21 09:36:15 +0400

    add "start" param to get_clients methods&fix url for attaches

--- api/urls.py
+++ api/urls.py
@@ -50,14 +50,15 @@ urlpatterns = patterns('',
                            name='edit_claim_for_other_managers'),
                        url(r'^claims/delete/$', 'base_api.views.delete_claim', name='delete_claim'),
                        url(r'^your_uploads/', include('multiuploader.urls')),
-                       url(r'^uploads/',  'base_api.views.my_view', name='my_view'),
+                       url(r'^uploads/', 'base_api.views.upload_files', name='my_view'),
                        url(r'^excel/',  'base_api.views.made_excel', name='made_excel'),
                        url(r'^robots.txt$', lambda r: HttpResponse("User-agent: *\nDisallow: /", mimetype="text/plain")),
-                       url(r'', 'base_api.views.page_not_found', name='404'),
+                       # url(r'', 'base_api.views.page_not_found', name='404'),
 )
 
 if settings.DEBUG:
     if settings.MEDIA_ROOT:
         urlpatterns += static(settings.MEDIA_URL,
-            document_root=settings.MEDIA_ROOT)
+                              document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns.append(url(r'', 'base_api.views.page_not_found', name='404'))
