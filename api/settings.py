28fe886d4c7b11b94535470adb85fff1512a98de
Janyell <e.sycheva_nn@mail.ru>
2015-02-23 20:50:34 +0300

    fixed bugs in claims/clients;

--- api/settings.py
+++ api/settings.py
@@ -65,7 +65,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'base_for_api',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': '123',
     }
 }
 
