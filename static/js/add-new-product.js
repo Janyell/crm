1c3af69748c6537b872857604d039a5df21bc3ae
Janyell <e.sycheva_nn@mail.ru>
2015-05-23 18:49:04 +0300

    v. 2.5 - 1, half 2;

--- static/js/add-new-product.js
+++ static/js/add-new-product.js
@@ -15,13 +15,20 @@
 
 	$(document).on('click', '.plus', function(){
 		var $this = $(this);
+        var $price;
 		var product_id = $this.attr('data-productId');
 		var product_title;
+        var product_price = 0;
 		var number = 1;
 		if (product_id != null) {
 			if ($( '.select-product_' + product_id ).size() != 0)
 				return;
-			product_title = $this.parent().prev().text();
+            $price = $this.parent().prev();
+            product_price = $price.text();
+            if (!product_price) {
+                product_price = 0;
+            }
+			product_title = $price.prev().text();
 			select_product_select_option(product_id);
 			number = $('select.product-select option[value="' + product_id + '"]').attr('data-number');
             if (!number) {
@@ -30,12 +37,13 @@
 		}
 		else {
 			product_id = --ij;
-			product_title = '<input type="text" class="select-product__title" name="select-product__title_' + product_id + '" placeholder="Другое">'
+			product_title = '<input type="text" class="select-product__title" name="select-product__title_' + product_id + '" placeholder="Другое">';
             add_product_select_selected_option(product_id);
 		}
 		$('.select-product-table__plus').before('<tr class="select-product_' + product_id + '">' +
 			'<td>' + product_title + '</td>' +
-			'<td><input type="number" min="0" value="'+ number +'" step="1" class="select-product__number" name="select-product__number_' + product_id + '" />' +
+            '<td><input type="number" min="0" value="'+ product_price +'" step="1" class="select-product__price" name="select-product__price_' + product_id + '" /></td>' +
+			'<td><input type="number" min="0" value="'+ number +'" step="1" class="select-product__number" name="select-product__number_' + product_id + '" /></td>' +
 			'<td><span class="btn btn-danger minus pull-right" data-productId="' + product_id + '">-</span></td>' +
 		'</tr>'
 		);
