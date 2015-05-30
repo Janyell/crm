d0a7bc209a6f27433ab38ea231312a0a17e9d1d6
megge <ria6@yandex.ru>
2015-05-30 20:17:28 +0400

    аналитика, продукты. дни

--- base_api/full_views/analyze_products.py
+++ base_api/full_views/analyze_products.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.shortcuts import render, render_to_response
-from datetime import datetime
+from datetime import datetime, timedelta
 from base_api.full_views.order_views import right_money_format
 from base_api.models import *
 from base_api.form import *
@@ -37,162 +37,116 @@ def full_view_analyzed_product(request):
     else:
         out.update({'user_role': user_role})
     product_id = request.GET['id']
-    if 'period' in request.POST:
-        print(xcfvgbh)
-    if 'period' in request.GET:
-        type_of_period = request.GET['period']
-    else:
-        type_of_period = None
-    if 'graphic[]' in request.GET:
-        type_of_graphic = request.GET.getlist('graphic[]')
-    else:
-        type_of_graphic = []
-    current_time = str(datetime.now())
-    current_mounth = current_time[5:]
-    current_mounth = current_mounth[:2]
-    current_year = current_time[:4]
     analyzed_product = Products.objects.get(id=product_id)
     product_name = analyzed_product.title
-    product_in_orders = Order_Product.objects.filter(product_id=product_id, is_deleted=0)
-    period = []
-    period_str = ''
-    amount = []
-    sum_of_pr = []
-    all_sum = 0
-    all_amount = 0
-    if type_of_period == 'month':
-        long_mounths = ['01', '03', '05', '07', '08', '10', '12']
-        for i in range(28):
-            period.append(i+1)
-            amount.append(0)
-            sum_of_pr.append(0)
-        if current_mounth != '02':
-            period.append(29)
-            period.append(30)
-            amount.append(0)
-            amount.append(0)
-            sum_of_pr.append(0)
-            if current_mounth in long_mounths:
-                period.append(31)
+    if 'period' in request.POST:
+        type_of_period = request.POST['period']
+        start_period = type_of_period[:10]
+        end_period = type_of_period[-10:]
+    else:
+        type_of_period = None
+    if 'graphic[]' in request.POST:
+        type_of_graphic = request.POST.getlist('graphic[]')
+    else:
+        type_of_graphic = []
+    if request.method == 'POST':
+        current_time = str(end_period)
+        current_day = current_time[-2:]
+        current_mounth = current_time[5:]
+        current_mounth = current_mounth[:2]
+        current_year = current_time[:4]
+        product_in_orders = Order_Product.objects.filter(product_id=product_id, is_deleted=0)
+        period = []
+        period_str = ''
+        amount = []
+        sum_of_pr = []
+        all_sum = 0
+        all_amount = 0
+        # по часам
+        if start_period == end_period:
+            for i in range(24):
+                period.append(i+1)
                 amount.append(0)
                 sum_of_pr.append(0)
+            for pr in product_in_orders:
+                order = Orders.objects.filter(id=pr.order_id, is_deleted=0).first()
+                if order and order.bill_status == 2:
+                    data = str(order.order_date)
+                    data_mounth = data[5:]
+                    data_mounth = data_mounth[:2]
+                    data_day = data[8:]
+                    data_day = data_day[:2]
+                    data_year = data[:4]
+                    data_hour = data[:13]
+                    data_hour = data_hour[-2:]
+                    if data_year == current_year and data_mounth == current_mounth and data_day == current_day:
+                        amount[int(data_hour) - 1] = int(amount[int(data_hour) - 1] + pr.count_of_products)
+                        sum_of_pr[int(data_hour) - 1] = sum_of_pr[int(data_hour) - 1] + pr.count_of_products * pr.price
+                        all_sum = all_sum + pr.count_of_products * pr.price
+                        all_amount = all_amount + pr.count_of_products
+            period_str = period
+
+        # по дням
+        start_period_time = datetime.strptime(start_period, "%Y-%m-%d").date()
+        end_period_time = datetime.strptime(end_period, "%Y-%m-%d").date()
+        if (end_period_time - start_period_time).days < 61:
+            for i in range((end_period_time - start_period_time).days + 1):
+                period.append(start_period_time + timedelta(days=i))
+                amount.append(0)
+                sum_of_pr.append(0)
+            for pr in product_in_orders:
+                order = Orders.objects.filter(id=pr.order_id, is_deleted=0).first()
+                if order and order.bill_status == 2:
+                    data = order.order_date.date()
+                    if start_period_time < data and data < end_period_time:
+                        amount[(data - start_period_time).days] = int(amount[(data - start_period_time).days] + pr.count_of_products)
+                        sum_of_pr[(data - start_period_time).days] = sum_of_pr[(data - start_period_time).days] + pr.count_of_products * pr.price
+                        all_sum = all_sum + pr.count_of_products * pr.price
+                        all_amount = all_amount + pr.count_of_products
+            period_str = period
+
+        # по месяцам
+        start_period_time = datetime.strptime(start_period, "%Y-%m-%d").date()
+        end_period_time = datetime.strptime(end_period, "%Y-%m-%d").date()
+        for i in range((end_period_time - start_period_time).mo + 1):
+            period.append(start_period_time + timedelta(days=i))
+            amount.append(0)
+            sum_of_pr.append(0)
         for pr in product_in_orders:
             order = Orders.objects.filter(id=pr.order_id, is_deleted=0).first()
             if order and order.bill_status == 2:
-                data = str(order.order_date)
-                data_mounth = data[5:]
-                data_mounth = data_mounth[:2]
-                data_day = data[8:]
-                data_day = data_day[:2]
-                data_year = data[:4]
-                if data_year == current_year and data_mounth == current_mounth:
-                    amount[int(data_day) - 1] = int(amount[int(data_day) - 1] + pr.count_of_products)
-                    sum_of_pr[int(data_day) - 1] = sum_of_pr[int(data_day) - 1] + pr.count_of_products * pr.price
+                data = order.order_date.date()
+                if start_period_time < data and data < end_period_time:
+                    amount[(data - start_period_time).days] = int(amount[(data - start_period_time).days] + pr.count_of_products)
+                    sum_of_pr[(data - start_period_time).days] = sum_of_pr[(data - start_period_time).days] + pr.count_of_products * pr.price
                     all_sum = all_sum + pr.count_of_products * pr.price
                     all_amount = all_amount + pr.count_of_products
-        # period_str = str(period)[1:-1]
         period_str = period
-    elif type_of_period == 'year':
-        for i in range(12):
-            period.append(i+1)
-            amount.append(0)
-            sum_of_pr.append(0)
-        for pr in product_in_orders:
-            order = Orders.objects.filter(id=pr.order_id, is_deleted=0).first()
-            if order and order.bill_status == 2:
-                data = str(order.order_date)
-                data_mounth = data[5:]
-                data_mounth = data_mounth[:2]
-                data_year = data[:4]
-                if data_year == current_year:
-                    amount[int(data_mounth) - 1] = int(amount[int(data_mounth) - 1] + pr.count_of_products)
-                    sum_of_pr[int(data_mounth) - 1] = sum_of_pr[int(data_mounth) - 1] + pr.count_of_products * pr.price
-                    all_sum = all_sum + pr.count_of_products * pr.price
-                    all_amount = all_amount + pr.count_of_products
-        period_str = []
-        period_str.append('Январь')
-        period_str.append('Февраль')
-        period_str.append('Март')
-        period_str.append('Апрель')
-        period_str.append('Май')
-        period_str.append('Июнь')
-        period_str.append('Июль')
-        period_str.append('Август')
-        period_str.append('Сентябрь')
-        period_str.append('Октябрь')
-        period_str.append('Ноябрь')
-        period_str.append('Декабрь')
-    elif type_of_period == 'all':
-        all_date_of_orders = Order_Product.objects.filter(product_id=product_id, is_deleted=0).order_by('order_date')
-        first_data = str(all_date_of_orders[0].order_date)
-        first_data_mounth = first_data[5:]
-        first_data_mounth = first_data_mounth[:2]
-        first_data_year = first_data[:4]
-        amount_of_period = (int(current_year) - int(first_data_year)) * 12 + int(current_mounth)\
-                           - int(first_data_mounth) + 2
-        for i in range(amount_of_period):
-            period.append(i+1)
-            amount.append(0)
-            sum_of_pr.append(0)
-        for pr in product_in_orders:
-            order = Orders.objects.filter(id=pr.order_id, is_deleted=0).first()
-            if order and order.bill_status == 2:
-                data = str(order.order_date)
-                data_mounth = data[5:]
-                data_mounth = data_mounth[:2]
-                data_year = data[:4]
-                data_year_and_mounth = data[:7]
-                current_year_and_mounth = current_year + '-' + current_mounth
-                i = (int(current_year) - int(data_year)) * 12 + int(current_mounth) - int(data_mounth)
-                amount[i] = int(amount[i] + pr.count_of_products)
-                sum_of_pr[i] = int(sum_of_pr[i] + pr.count_of_products * pr.price)
-                all_sum = all_sum + pr.count_of_products * pr.price
-                all_amount = all_amount + pr.count_of_products
-        period_str = []
-        for i in range(amount_of_period):
-            if ((i+int(first_data_mounth)-1) % 12) == 1:
-                period_str.append('Январь')
-            elif ((i+int(first_data_mounth)-1) % 12) == 2:
-                period_str.append('Февраль')
-            elif ((i+int(first_data_mounth)-1) % 12) == 3:
-                period_str.append('Март')
-            elif ((i+int(first_data_mounth)-1) % 12) == 4:
-                period_str.append('Апрель')
-            elif ((i+int(first_data_mounth)-1) % 12) == 5:
-                period_str.append('Май')
-            elif ((i+int(first_data_mounth)-1) % 12) == 6:
-                period_str.append('Июнь')
-            elif ((i+int(first_data_mounth)-1) % 12) == 7:
-                period_str.append('Июль')
-            elif ((i+int(first_data_mounth)-1) % 12) == 8:
-                period_str.append('Август')
-            elif ((i+int(first_data_mounth)-1) % 12) == 9:
-                period_str.append('Сентябрь')
-            elif ((i+int(first_data_mounth)-1) % 12) == 10:
-                period_str.append('Октябрь')
-            elif ((i+int(first_data_mounth)-1) % 12) == 11:
-                period_str.append('Ноябрь')
-            elif ((i+int(first_data_mounth)-1) % 12) == 0:
-                period_str.append('Декабрь')
-        amount = amount[::-1]
-        sum_of_pr = sum_of_pr[::-1]
-    amount_str = str(amount)[1:-1]
-    sum_str = str(sum_of_pr)[1:-1]
-    if all_amount == 0:
-        average_sum = 0
-    else:
-        average_sum = all_sum / all_amount
-    average_sum_right_format = right_money_format(average_sum)
-    out.update({'page_title': "Анализ продаж продукта"})
-    out.update({'select_period': period_str})
-    if 'number' in type_of_graphic:
-        out.update({'number_data': amount_str})
-    if 'sum' in type_of_graphic:
-        out.update({'sum_data': sum_str})
-    out.update({'product_name': product_name})
-    out.update({'period': type_of_period})
-    out.update({'graphic': type_of_graphic})
-    out.update({'average_sum_right_format': average_sum_right_format})
+
+        amount_str = str(amount)[1:-1]
+        sum_str = str(sum_of_pr)[1:-1]
+        if all_amount == 0:
+            average_sum = 0
+        else:
+            average_sum = all_sum / all_amount
+        average_sum_right_format = right_money_format(average_sum)
+        out.update({'select_period': period_str})
+        if 'number' in type_of_graphic:
+            out.update({'number_data': amount_str})
+        if 'sum' in type_of_graphic:
+            out.update({'sum_data': sum_str})
+        out.update({'period': type_of_period})
+        out.update({'graphic': type_of_graphic})
+        out.update({'average_sum_right_format': average_sum_right_format})
     user_role = Roles.objects.get(id=request.user.id).role
+    out.update({'page_title': "Анализ продаж продукта"})
+    out.update({'product_name': product_name})
     out.update({'user_role': user_role})
     return render(request, 'view_analyzed_product.html', out)
+
+
+def month_delta(date, delta):
+    m, y = (date.month+delta) % 12, date.year + ((date.month)+delta-1) // 12
+    if not m: m = 12
+    d = min(date.day, [31, 29 if y%4==0 and not y%400==0 else 28,31,30,31,30,31,31,30,31,30,31][m-1])
+    return date.replace(day=d,month=m, year=y)
