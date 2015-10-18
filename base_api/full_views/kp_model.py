83cddfc33f977d7443062d02de7fb3b1b04a7ed1
megge <ria6@yandex.ru>
2015-10-18 12:52:28 +0400

    попытка №2 ^^

--- base_api/full_views/kp_model.py
+++ base_api/full_views/kp_model.py
@@ -1,45 +1,51 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 from django.http import HttpResponseRedirect
-from docx import Document
-from docx.shared import Inches
+from subprocess import Popen
+from api.settings import MEDIA_ROOT
 
 
 def full_generate_kp(request):
-    document = Document()
+    filename = MEDIA_ROOT + '/' + str('uploads/test.html')
+    out_filename = MEDIA_ROOT + '/' + str('uploads/ttt1.docx')
+    out_filename_pdf = MEDIA_ROOT + '/' + str('uploads/ttt1.pdf')
+    # Popen(['pandoc', filename, '-f', 'html', '-t', 'docx', '-s', '-o', out_filename])
+    # Popen(['pandoc', filename, '-f', 'html', '-s', '-o', out_filename_pdf])
 
-    document.add_heading('Document Title', 0)
-
-    p = document.add_paragraph('A plain paragraph having some ')
-    p.add_run('bold').bold = True
-    p.add_run(' and some ')
-    p.add_run('italic.').italic = True
-
-    document.add_heading('Heading, level 1', level=1)
-    document.add_paragraph('Intense quote', style='IntenseQuote')
-
-    document.add_paragraph(
-        'first item in unordered list', style='ListBullet'
-    )
-    document.add_paragraph(
-        'first item in ordered list', style='ListNumber'
-    )
-
-    document.add_picture('/Users/megge/Documents/Яндекс.Диск/gif/tumblr_nata787Rh01rjm4kxo1_500.jpg', width=Inches(1.25))
-
-    table = document.add_table(rows=1, cols=3)
-    hdr_cells = table.rows[0].cells
-    hdr_cells[0].text = 'Qty'
-    hdr_cells[1].text = 'Id'
-    hdr_cells[2].text = 'Desc'
-    row_cells = table.add_row().cells
-    row_cells[0].text = str(3123)
-    row_cells[1].text = str(3213)
-    row_cells[2].text = str(2313213)
-
-    document.add_page_break()
-
-    document.save('demo.docx')
+    # document = Document()
+    #
+    # document.add_heading('Document Title', 0)
+    #
+    # p = document.add_paragraph('A plain paragraph having some ')
+    # p.add_run('bold').bold = True
+    # p.add_run(' and some ')
+    # p.add_run('italic.').italic = True
+    #
+    # document.add_heading('Heading, level 1', level=1)
+    # document.add_paragraph('Intense quote', style='IntenseQuote')
+    #
+    # document.add_paragraph(
+    #     'first item in unordered list', style='ListBullet'
+    # )
+    # document.add_paragraph(
+    #     'first item in ordered list', style='ListNumber'
+    # )
+    #
+    # document.add_picture('/Users/megge/Documents/Яндекс.Диск/gif/tumblr_nata787Rh01rjm4kxo1_500.jpg', width=Inches(1.25))
+    #
+    # table = document.add_table(rows=1, cols=3)
+    # hdr_cells = table.rows[0].cells
+    # hdr_cells[0].text = 'Qty'
+    # hdr_cells[1].text = 'Id'
+    # hdr_cells[2].text = 'Desc'
+    # row_cells = table.add_row().cells
+    # row_cells[0].text = str(3123)
+    # row_cells[1].text = str(3213)
+    # row_cells[2].text = str(2313213)
+    #
+    # document.add_page_break()
+    #
+    # document.save('demo.docx')
 
     # import os
     # from win32com import client
