3eb916e707b452fae73134304a26105e13fd80b1
Janyell <e.sycheva_nn@mail.ru>
2016-05-27 21:23:38 +0300

    added close-claim funcs;

--- base_api/views.py
+++ base_api/views.py
@@ -1538,3 +1538,27 @@ def get_tasks(request):
 
 def close_claim(request):
     pass
+
+
+def get_reasons(request):
+    return render(request, 'setting/get_reasons.html')
+
+
+def edit_reason(request):
+    pass
+
+
+def delete_reason(request):
+    pass
+
+
+def massive_activate_reasons(request):
+    pass
+
+
+def massive_deactivate_reasons(request):
+    pass
+
+
+def massive_delete_reasons(request):
+    pass
